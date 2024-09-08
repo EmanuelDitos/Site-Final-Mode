7fb7b96344c3a4af545de8d1bd7f79559716de68
Gabriel Berlucci <gabrielberluccidev@proton.me>
2024-09-08 01:50:53 -0300

    feat: add scripts to open new window for search bar

---
+++ scripts/searchBarPages.js
@@ -0,0 +1,12 @@
+const searchInput = document.getElementById("search_input");
+let suggestions = ["manual", "suporte", "treinamento"];
+
+searchInput.addEventListener("input", (e) => {
+  let tab = e.target.value.toLowerCase();
+
+  for (let i = 0; i < tab.length; i++) {
+    if (tab.includes(suggestions[i])) {
+      window.open(`../${suggestions[i]}-page/${suggestions[i]}.html`);
+    }
+  }
+});
