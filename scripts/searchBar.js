69b362aa73f2cdf56ee6cb9488efe42294512932
Gabriel Berlucci <gabrielberluccidev@proton.me>
2024-09-12 21:03:47 -0300

    feat: add a new feature that see the document name and open the path based on the name

--- scripts/searchBar.js
+++ scripts/searchBar.js
@@ -3,13 +3,23 @@ let suggestions = ["manual", "suporte", "treinamento"];
 
 searchInput.addEventListener("input", (e) => {
   let tab = e.target.value.toLowerCase();
+  let path = window.location.pathname;
+  let page = path.split("/").pop();
+  console.log(page);
 
   for (let i = 0; i < tab.length; i++) {
     if (tab.includes(suggestions[i])) {
-      window.open(
-        `./pages/${suggestions[i]}-page/${suggestions[i]}.html`,
-        "_self"
-      );
+      if (page === "index.html") {
+        window.open(
+          `./pages/${suggestions[i]}-page/${suggestions[i]}.html`,
+          "_self"
+        );
+      } else {
+        window.open(
+          `../${suggestions[i]}-page/${suggestions[i]}.html`,
+          "_self"
+        );
+      }
     }
   }
 });
